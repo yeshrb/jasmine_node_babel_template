c1e8fc1808bd64dc626c9164b981da18f64b9b40
ljj <yeshrb@126.com>
2018-04-02 09:14:17 +0800

    add spec for ch03

---
+++ dist/src/object.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//Object Literals
+var empty_object = {};
+var stooge = {
+  "first-name": "Jerome",
+  "last-name": "Howard"
+};
+var flight = {
+  airline: "Oceanic",
+  number: 815,
+  departure: {
+    IATA: "SYD",
+    time: "2004-09-22 14:55",
+    city: "Sydney"
+  },
+  arrival: {
+    IATA: "LAX",
+    time: "2004-09-23",
+    city: "Los Angeles"
+  }
+};
+//# sourceMappingURL=object.js.map
---
+++ spec/03-objectsSpec.js
@@ -0,0 +1,38 @@
+describe("第三章 对象", function() {
+  var empty_object = {};
+  var stooge = {
+    "first-name" : "Jerome",
+    "last-name" : "Howard",
+  };
+  var flight = {
+    airline: "Oceanic",
+    number:815,
+    departure:{
+      IATA: "SYD",
+      time: "2004-09-22 14:55",
+      city: "Sydney",
+    },
+    arrival: {
+      IATA: "LAX",
+      time: "2004-09-23",
+      city: "Los Angeles"
+    }
+  };
+  describe("检索" ,function () {
+    it('使用形如["str"]可以获得对象属性' ,function () {
+      expect(stooge["first-name"]).toEqual("Jerome");
+    });
+
+    it('使用形如Obj.key1可以获得对象属性' ,function () {
+      expect(flight.airline).toEqual("Oceanic");
+    });
+    it('不能使用形如Obj."key1"获得对象属性' ,function () {
+      expect(() => stooge."first-name").toBe(true);
+    });
+
+  });
+
+
+});
+
+
